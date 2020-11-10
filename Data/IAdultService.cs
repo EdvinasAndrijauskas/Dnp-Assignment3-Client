bcbd99d254d3971b70c8a7db6dc8c9353feb5074
unknown <edvinas.andrijauskas2000@gmail.com>
2020-11-10 13:56:19 +0100

    done assignment

---
+++ Data/IAdultService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Managing_Adults.Data.Impl;
+using Models;
+
+namespace Managing_Adults.Data
+{
+    public interface IAdultService
+    {
+        Task<IList<Adult>> GetAdultAsync();
+        Task<HttpStatusCode> AddAdult(Adult adult);
+        Task RemoveAdultAsync(int adultId);
+  
+
+    }
+}
---
+++ Data/Impl/AdultService.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using Models;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+namespace Managing_Adults.Data.Impl
+{
+    public class AdultService : IAdultService
+    {
+        private string uri = "https://localhost:5003";
+        private readonly HttpClient client;
+
+        public AdultService()
+        {
+            client = new HttpClient();
+        }
+
+        public async Task<IList<Adult>> GetAdultAsync()
+        {
+            Task<string> stringAsync = client.GetStringAsync(uri + "/Adults");
+            string message = await stringAsync;
+            List<Adult> result = JsonSerializer.Deserialize<List<Adult>>(message);
+            return result;
+        }
+
+        public async Task<HttpStatusCode> AddAdult(Adult adult)
+        {
+            string familySerialized = JsonSerializer.Serialize(adult);
+
+            StringContent content = new StringContent(
+                familySerialized,
+                Encoding.UTF8,
+                MediaTypeNames.Application.Json
+            );
+            HttpResponseMessage responseMessage = await client.PostAsync("https://localhost:5003/Adults", content);
+            return responseMessage.StatusCode;
+        }
+
+        public async Task AddAdultAsync(Adult adult)
+        {
+            string todoSerialized = JsonSerializer.Serialize(adult);
+            HttpContent content = new StringContent(todoSerialized,
+                Encoding.UTF8,
+                "application/json");
+            await client.PutAsync("http://dnp.metamate.me/Adults", content);
+        }
+
+        public async Task RemoveAdultAsync(int adultId)
+        {
+            await client.DeleteAsync($"{uri}/Adults/{adultId}");
+        }
+    }
+}
